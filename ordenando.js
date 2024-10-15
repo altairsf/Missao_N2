650df864f0d8d38317900f840df841103a7c4ac5
altairsf <altair.regina.26@gmail.com>
2024-10-15 00:05:55 -0300

    Add files via upload

---
+++ ordenando.js
@@ -0,0 +1,23 @@
+const swap=([a,b],pos) => {
+    
+}
+
+const shuffle=([a,b],QtTr) =>{
+
+}
+
+const bubble_sort=([]) =>{
+
+}
+
+const selection_sort=[] =>{
+
+}
+
+const quick_sort=([],IniPos, FinPos) =>{
+
+}
+
+const particionamento=([], IniPos, FinPos,pivot) =>{
+
+}
